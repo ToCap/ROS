57e46f76411ea1523740db83fe7a7aec5635e742
Tocap <thomas.barbillon@gmail.com>
2025-10-14 22:45:51 +0200

    fix : preparse URDF file before launch

--- src/sensor_touch_interface/launch/module_launch.py
+++ src/sensor_touch_interface/launch/module_launch.py
@@ -5,16 +5,21 @@ from launch_ros.actions import Node
 from launch.substitutions import PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import TimerAction
+import xacro
 
 import os
 
 def generate_launch_description():
+    
     # --- Paths ---
     pkg_share = get_package_share_directory('sensor_touch_interface')
 
     urdf_file = os.path.join(pkg_share, 'description', 'sensor_touch_interface.urdf.xacro')
     controllers_file = os.path.join(pkg_share, 'config', 'sensor_touch_interface.yaml')
     sdf_file = os.path.join(pkg_share, 'description', 'touch_bot.sdf')
+
+    # 
+    urdf_content = xacro.process_file(urdf_file).toxml()
 
     # Start Gazebo simulation (Ignition)
     gz_sim = ExecuteProcess(
@@ -27,7 +32,7 @@ def generate_launch_description():
     spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
-        arguments=['-file', urdf_file, '-name', 'touch_bot'],
+        arguments=['-string', urdf_content, '-name', 'touch_bot'],
         output='screen'
     )
     # spawn_entity = TimerAction(
@@ -46,7 +51,7 @@ def generate_launch_description():
     control_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
-        parameters=[{'robot_description': open(urdf_file).read()},
+        parameters=[{'robot_description': urdf_content},
                     controllers_file],
         output='screen',
     )
